646559378784787d3537f140015208d45ad7ad45
TiagoFSDC <126723661+TiagoFSDC@users.noreply.github.com>
2023-04-02 22:38:40 -0300

    Correcao da sintaxe

--- SQLQuery_2.sql
+++ SQLQuery_2.sql
@@ -4,7 +4,7 @@ create table Cliente(
     CodCli int NOT NULL,
     NomeCli varchar(50) NOT NULL,
     CONSTRAINT PK_Cliente primary key(CodCli)
-);
+)
 
 create table Pedido(
     NoPed int not null,
@@ -13,7 +13,7 @@ create table Pedido(
 
     CONSTRAINT PK_Pedido primary key(NoPed),
     constraint FK_Pedido_Cliente foreign key(CodCli) REFERENCES Cliente(CodCli)
-);
+)
 
 
 create table Pedido_Fone(
@@ -22,14 +22,14 @@ create table Pedido_Fone(
 
     CONSTRAINT PK_Pedido_Fone primary key(NoPedido,NoTel),
     constraint FK_Pedido_Fone_Pedido foreign key(NoPedido) REFERENCES Pedido(NoPed)
-);
+)
 
 create table Especificacao_Peca(
     CodPecaEsp int NOT NULL,
     descPeca int NOT NULL,
 
     CONSTRAINT PK_Especificacaco_Peca primary key(CodPecaEsp),
-);
+)
 
 create table Item_Pedido(
     NPed int NOT NULL,
